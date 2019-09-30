609e537565f19320bd0d30139b83fe7043a2973c
hoj2 <hoj2@WIT45001220.wit.private>
2019-09-30 10:43:55 -0400

    readying tester method to be able to retry after finding errors.

--- Lab 2 Comp 2000/src/src_stack/Testing.java
+++ Lab 2 Comp 2000/src/src_stack/Testing.java
@@ -1,6 +1,5 @@
 package src_stack;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Testing {
@@ -9,19 +8,15 @@ public class Testing {
 		Scanner input = new Scanner(System.in);
 		String temp = "";
 		System.out.printf("Input 1 to print pre-coded test messages, 2 to test your own infixes: ");
-		try {
+		while(!(temp.equals("1")) && !(temp.equals("2"))) {
 			temp = input.next();
-		} catch (InputMismatchException e) {
-			System.out.printf("Enter a valid input!%n");
-			System.exit(0);
-		}
-		if (temp.equals("1")) {
-			one();
-		} else if (temp.equals("2")) {
-			two();
-		} else {
-			System.out.printf("Enter a valid input!%n");
-			System.exit(0);
+			if (temp.equals("1")) {
+				one();
+			} else if (temp.equals("2")) {
+				two();
+			} else {
+				System.out.printf("Enter a valid input!(1 for premade code, 2 for custom equation)%n");
+			}	
 		}
 		input.close();
 
@@ -46,41 +41,45 @@ public class Testing {
 		System.out.printf("Postfix String: %s%n",temp);
 		
 		// Converts postfix to double and saves to ans
-		double ans = PostfixEvaluator.calculate(temp);
-		
-		// Holds answer as string for formatting decimal places
-		String ansString;
-		if(ans%1==0) {
-			ansString = String.format("%.0f", ans);
-		}else {
-			ansString = String.format("%f", ans);
-			int latestZero = 0;
-			boolean foundZero = false;
+		try {
+			double ans = PostfixEvaluator.calculate(temp);
 			
-			//Removes any extra zero's that are remaining in decimal
-			for(int i = 0; i < ansString.length(); i++) {
-				//Starts checking after decimal
-				if(ansString.charAt(i) == '.') {
-					for(int j = i+1; j < ansString.length(); j++) {
-						if(ansString.charAt(j) == '0' && !foundZero) {
-							latestZero = j;
-							foundZero = true;
-						}else if(!(ansString.charAt(j) == '0')){
-							latestZero = 0;
-							foundZero = false;
+			// Holds answer as string for formatting decimal places
+			String ansString;
+			if(ans%1==0) {
+				ansString = String.format("%.0f", ans);
+			}else {
+				ansString = String.format("%f", ans);
+				int latestZero = 0;
+				boolean foundZero = false;
+				
+				//Removes any extra zero's that are remaining in decimal
+				for(int i = 0; i < ansString.length(); i++) {
+					//Starts checking after decimal
+					if(ansString.charAt(i) == '.') {
+						for(int j = i+1; j < ansString.length(); j++) {
+							if(ansString.charAt(j) == '0' && !foundZero) {
+								latestZero = j;
+								foundZero = true;
+							}else if(!(ansString.charAt(j) == '0')){
+								latestZero = 0;
+								foundZero = false;
+							}
 						}
+						break;
 					}
-					break;
+				}
+				
+				//Removes extra 0's
+				if(latestZero != 0 && foundZero) {
+					ansString = ansString.substring(0, latestZero);
 				}
 			}
 			
-			//Removes extra 0's
-			if(latestZero != 0 && foundZero) {
-				ansString = ansString.substring(0, latestZero);
-			}
+			System.out.printf("Postfix evaluation: %s%n", ansString);
+		}catch(Exception e) {
 		}
 		
-		System.out.printf("Postfix evaluation: %s%n", ansString);
 		
 		System.out.printf("Test another infix? Enter y to go again, anything else to exit: ");
 		String yn = input.next();
